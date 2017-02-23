6d9b3af074c6e03085614998861e52ae6f074a70
Alexander Böhn <fish2000@gmail.com>
2017-02-23 08:25:54 -0500

    Added octype_t<…> and ocpointer_t<…> type-traitlet templates ... these are used to force either the pointer or non-pointer typename of     an Objective-C type. For example:
        using T = NSString;		    // typename without pointer
        using octype = octype_t<T>;     // octype is “NSString”
        using ocptr = ocpointer_t<T>;   // ocptr is “NSString*”
    
        using U = NSData*;		    // typename plus pointer
        using octype = octype_t<U>;     // octype is “NSData”
        using ocptr = ocpointer_t<U>;   // ocptr is “NSData*”

--- include/subjective-c/appkit.hh
+++ include/subjective-c/appkit.hh
@@ -13,22 +13,32 @@ namespace objc {
     namespace appkit {
         
         template <typename OCType>
+        using ocpointer_t = typename std::decay_t<std::conditional_t<
+                                                  std::is_pointer<OCType>::value, OCType,
+                                                               std::add_pointer_t<OCType>>>;
+        
+        template <typename OCType>
+        using octype_t = typename std::decay_t<std::conditional_t<
+                                               std::is_pointer<OCType>::value,
+                                               std::remove_pointer_t<OCType>, OCType>>;
+        
+        template <typename OCType>
         BOOL can_paste(NSPasteboard* board = nil) noexcept {
-            static_assert(objc::traits::is_object<std::add_pointer_t<OCType>>::value,
+            static_assert(objc::traits::is_object<ocpointer_t<OCType>>::value,
                           "objc::appkit::can_paste<OCType> requires an Objective-C object type");
             if (!board) { board = [NSPasteboard generalPasteboard]; }
-            return [board canReadObjectForClasses:@[ [OCType class] ]
+            return [board canReadObjectForClasses:@[ [octype_t<OCType> class] ]
                                           options:@{}];
         }
         
         template <typename OCType>
         __attribute__((ns_returns_retained))
-        typename std::add_pointer_t<OCType> paste(NSPasteboard* board = nil) noexcept {
-            static_assert(objc::traits::is_object<std::add_pointer_t<OCType>>::value,
+        ocpointer_t<OCType> paste(NSPasteboard* board = nil) noexcept {
+            static_assert(objc::traits::is_object<ocpointer_t<OCType>>::value,
                           "objc::appkit::paste<OCType> requires an Objective-C object type");
             if (!board) { board = [NSPasteboard generalPasteboard]; }
-            if (!can_paste<OCType>(board)) { return nil; }
-            NSArray* out = [board readObjectsForClasses:@[ [OCType class] ]
+            if (!can_paste<ocpointer_t<OCType>>(board)) { return nil; }
+            NSArray* out = [board readObjectsForClasses:@[ [octype_t<OCType> class] ]
                                                 options:@{}];
             
             /// array is nil on error -- but empty if the call to
